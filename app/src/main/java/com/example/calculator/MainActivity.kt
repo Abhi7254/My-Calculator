22b9e64f519e004f76225d9ed4a9a56b4574ba67
Abhishek Bhardwaj <abhishekbhardwaj9199@gmail.com>
2023-05-16 21:52:56 +0530

    Backend part of the project is initilized and added some functons to it.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,64 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    // Represent whether the lastly pressed key is numeric or not
+    var lastNumeric: Boolean = false
+
+    // If true, do not allow to add another DOT
+    var lastDot: Boolean = false
+
+    private var tvInput:TextView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvInput = findViewById(R.id.tvInput)
     }
+
+    fun onDigit(view: View){
+        tvInput?.append((view as Button).text)
+        lastNumeric = true
+    }
+
+    fun onClear(view: View){
+        tvInput?.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+    /**
+     * Append . to the TextView
+     */
+    fun onDecimalPoint(view: View) {
+
+        // If the last appended value is numeric then append(".") or don't.
+        if (lastNumeric && !lastDot) {
+            tvInput?.append(".")
+            lastNumeric = false // Update the flag
+            lastDot = true // Update the flag
+        }
+    }
+
+    /**
+     * Append +,-,*,/ operators to the TextView as per the Button.Text
+     */
+    fun onOperator(view: View) {
+        tvInput?.text?.let {
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
+                tvInput?.append((view as Button).text)
+                lastNumeric = false // Update the flag
+                lastDot = false    // Reset the DOT flag
+            }
+        }
+
+    }
+
+    /**
+     * Calculate the output
+     */
+
+
 }
